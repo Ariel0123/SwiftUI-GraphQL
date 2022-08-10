3b8f63a18c1b571fc7ac498b9e3fa392a6720071
Ariel Ortiz <arielortiz.dev@gmail.com>
2022-08-09 19:57:32 -0700

    Initial Commit

---
+++ TasksGraphQL/TasksGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksGraphQLApp.swift
+//  TasksGraphQL
+//
+//  Created by Ariel Ortiz on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TasksGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
